5e4f803e167ef643dda105cc62b01cbf3a062107
manfromexistence.reacts <manfromexistence.reacts@gmail.com>
2025-05-22 12:43:45 +0000

    feat: friday

---
+++ integrations/www/app/page.tsx
@@ -0,0 +1,10 @@
+import { QuickCustomizer } from "@/components/customizer/quick-customizer";
+
+export default function Page() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <span>Hello</span>
+      {/* <QuickCustomizer /> */}
+    </div>
+  );
+}
