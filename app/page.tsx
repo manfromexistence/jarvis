864236dd3567c72f29908fbcff10246b94495d57
manfromexistence.reacts <manfromexistence.reacts@gmail.com>
2025-05-22 14:29:46 +0000

    feat: hi

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { QuickCustomizer } from "@/components/customizer/quick-customizer";
 
 export default function Page() {
   return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <span>Hello</span>
-      {/* <QuickCustomizer /> */}
+    <div className="h-screen w-full p-4">
+      {/* <span>Hello</span> */}
+      <QuickCustomizer />
     </div>
   );
 }
