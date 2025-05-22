53965eed061dfcea998251bc2012ea3262dc0260
manfromexistence.reacts <manfromexistence.reacts@gmail.com>
2025-05-22 12:57:25 +0000

    feat: friday

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-export default function Home() {
+import { QuickCustomizer } from "@/components/customizer/quick-customizer";
+
+export default function Page() {
   return (
-    <h1>Hello, How are you?</h1>
+    <div className="h-screen w-full flex items-center justify-center">
+      <span>Hello</span>
+      {/* <QuickCustomizer /> */}
+    </div>
   );
 }
