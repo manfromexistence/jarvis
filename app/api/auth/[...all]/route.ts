622841dde107ca72378167a0fd0491ed57e5967b
manfromexistence.reacts <manfromexistence.reacts@gmail.com>
2025-05-23 01:37:43 +0000

    feat: better auth is not working

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,4 +1,10 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
- 
-export const { GET, POST } = toNextJsHandler(auth.handler);
+import { NextRequest } from "next/server";
+
+export const { GET } = toNextJsHandler(auth);
+
+export const POST = async (req: NextRequest) => {
+	const res = await auth.handler(req);
+	return res;
+};
