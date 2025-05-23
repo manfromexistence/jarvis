b8449aa9bd213614fc1b8a95b4e59dbc0a264f1d
manfromexistence.reacts <manfromexistence.reacts@gmail.com>
2025-05-23 13:43:58 +0000

    feat: better auth auth client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -6,7 +6,31 @@ export const authClient = createAuthClient({
         anonymousClient()
     ],
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://9000-firebase-jarvis-1747923459525.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev/?monospaceUid=134891&embedded=0"
+    baseURL: "http://localhost:3000"
+    // baseURL: "https://9000-firebase-jarvis-1747923459525.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev/?monospaceUid=134891&embedded=0"
 })
 
-export const { signIn, signUp, signOut, useSession } = createAuthClient()
+export const { 
+    changeEmail, 
+    changePassword, 
+    deleteUser, 
+    forgetPassword, 
+    getAccessToken, 
+    getSession, 
+    linkSocial, 
+    listAccounts, 
+    listSessions, 
+    refreshToken, 
+    resetPassword, 
+    sendVerificationEmail, 
+    revokeOtherSessions, 
+    revokeSession, 
+    revokeSessions, 
+    unlinkAccount, 
+    updateUser, 
+    verifyEmail,
+    signIn, 
+    signUp, 
+    signOut,
+    useSession 
+} = createAuthClient()
