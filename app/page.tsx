5514e619753943dcd376bb635bc541949f9f9772
manfromexistence.reacts <manfromexistence.reacts@gmail.com>
2025-05-23 10:48:22 +0000

    feat: working on sign in box

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { QuickCustomizer } from "@/components/customizer/quick-customizer";
 
 export default function Page() {
   return (
-    <div className="min-h-screen w-full p-4 ">
-      <span>Hello, guys.</span>
+    <div className="min-h-screen w-full flex items-center justify-center flex-col gap-4 p-4">
+      {/* <span>Hello, guys.</span> */}
       <SignIn />
       <SignUp />
       {/* <QuickCustomizer /> */}
